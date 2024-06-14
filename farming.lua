6de4fe50950425bbf1ec7b4b2854a3f98a0ab44a
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-06-15 02:30:23 +1000

    Update farming.lua

--- farming.lua
+++ farming.lua
@@ -2,60 +2,56 @@ local function cropHandling()
  local success, data = turtle.inspectDown()
  if success then
   if data.state.age == 3 then
-   assert(turtle.digDown())
-   assert(turtle.placeDown())
-   assert(turtle.suckDown())
-  else
-
+   turtle.digDown()
   end
  end
 end
 
 local function fuelHandling()
- if turtle.getFuelLevel() < 100 then
- assert(turtle.refuel())
- end
-end
-
-local function chestHandling()
- local has_block, data = turtle.inspect()
- if has_block then
-  if data.name == "minecraft:chest" then
-   for i =  2, 16 do
-    assert(turtle.drop())
-   end
-  end
- end
-end
-
-local function fuelStorageHandling()
  local has_block, data = turtle.inspect()
  if has_block then
   if data.name == "computercraft:wired_modem_full" then
   local modem = peripheral.wrap("front")
   local turtleName = modem.getNameLocal()
-  local chest = peripheral.find("chest")
-  assert(chest.pushItems(turtleName, 1, 1))
+  local chest = peripheral.find("minecraft:chest")
+   if turtle.getFuelLevel() < 800 then
+   chest.pushItems(turtleName, 1, 1, 16)
+   turtle.select(16)
+   turtle.refuel()
+   end
   end
  end
 end
-    
-LENGTH = 17
+              
 T = 1
 
+local length = 17
+local moves = length
 while (T>0) do
-  local mLength = LENGTH
-  for x = 1, 2 do
-    for i = 1, mLength do
-      cropHandling()
-      fuelHandling()
-      chestHandling()
-      fuelStorageHandling()
-      assert(turtle.forward())
-    end
-  assert(turtle.turnLeft())
-  mLength = mLength - 1
+if length == 0 then
+ assert(shell.execute("go", "forward", "9"))
+ fuelHandling()
+ assert(turtle.turnLeft())
+ assert(shell.execute("go", "forward", "8"))
+ assert(turtle.turnLeft())
+ sleep(1)
+ length = 17
+ moves = length
+ return
+ else
+ for x = 1, 3 do
+  for i = 1, length do
+   moves = moves - 1
+   cropHandling()
+   assert(turtle.forward())
+   if moves == 0 then
+    assert(turtle.turnLeft())
+    moves = length
+   end
   end
+ end
+end   
+length = length - 1
+print(length)
 end
  
-
