222622af4fbc255b26d1d108e45c555b9db94796
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-06-21 00:28:18 +1000

    Create tracking-server.lua

---
+++ tracking-server.lua
@@ -0,0 +1,31 @@
+term.clear()
+term.setCursorPos(1, 1)
+ 
+peripheral.find("modem", rednet.open)
+ 
+local monitor = peripheral.find("monitor")
+monitor.setTextScale(0.5)
+ 
+-- init table to store messages from different IDs
+local messages = {}
+ 
+-- main loop
+while true do
+  -- wait for any message
+  local senderId, message, protocol = rednet.receive("tracking")
+ 
+  -- update latest message for the sender's ID
+  messages[senderId] = message
+ 
+  -- clear console
+  term.clear()
+  term.setCursorPos(1, 1)
+ 
+  -- print the latest messages from each ID
+  for id, msg in pairs(messages) do
+    print(msg)
+  end
+ -- output to monitor
+ term.redirect(monitor)
+end
+ 
