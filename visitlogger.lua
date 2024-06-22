13a091d26c89f8aed0e8f8269b4f4bc9f4accff9
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-06-22 23:29:53 +1000

    Update visitlogger.lua

--- visitlogger.lua
+++ visitlogger.lua
@@ -6,18 +6,19 @@ logfile = fs.open("logs.txt", "a")
 local scanner = peripheral.find("universal_scanner")
 
 while true do
-local log = os.date("%d.%m.%y %H:%M:%S")
+local log = ""
 local data = scanner.scan("player", 6)
 if data ~= nil then
  for i = 1, #data do
   if data[i] ~= nil then
    if data[i]["uuid"] ~= OWNER then
     local coords = location:add(vector.new(data[i]["x"], data[i]["y"], data[i]["z"]))
-    log = log .. "\n" .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
+    log = log .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
    end
   end
  end
- if log ~= os.date("%d.%m.%y %H:%M:%S") then
+ if log ~= "" then
+  logfile.writeLine(os.date("%d.%m.%y %H:%M:%S"))
   logfile.writeLine(log)
   logfile.flush()
  end
