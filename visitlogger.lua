dd128b44c454ffa261a0c0f8006f862c7a9c9fff
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-06-22 22:26:32 +1000

    Update visitlogger.lua

--- visitlogger.lua
+++ visitlogger.lua
@@ -1,3 +1,5 @@
+OWNER = "" -- insert UUID
+
 scanner = peripheral.find("universal_scanner")
 location = vector.new(0, 0, 0) -- enter computer location here
 logfile = fs.open("logs.txt", "a")
@@ -8,8 +10,10 @@ local data = scanner.scan("player", 6)
 if data ~= nil then
  for i = 1, #data do
   if data[i] ~= nil then
-   local coords = location:add(vector.new(data[i]["x"], data[i]["y"], data[i]["z"]+1)) -- +1 to z because the scanner coordinate for Z is inaccurate.
-   log = log .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
+   if data[i]["uuid"] ~= OWNER then
+    local coords = location:add(vector.new(data[i]["x"], data[i]["y"], data[i]["z"]+1)) -- +1 to z because the scanner coordinate for Z is inaccurate.
+    log = log .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
+   end
   end
  end
  if log ~= "" then
