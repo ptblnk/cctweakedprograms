7ada8015ddedc75a000d6972ea3a084c02b00ad2
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-06-22 22:32:09 +1000

    Update visitlogger.lua

--- visitlogger.lua
+++ visitlogger.lua
@@ -5,19 +5,18 @@ location = vector.new(0, 0, 0) -- enter computer location here
 logfile = fs.open("logs.txt", "a")
  
 while true do
-local log = ""
+local log = os.date("%d.%m.%y %H:%M:%S")
 local data = scanner.scan("player", 6)
 if data ~= nil then
  for i = 1, #data do
   if data[i] ~= nil then
    if data[i]["uuid"] ~= OWNER then
     local coords = location:add(vector.new(data[i]["x"], data[i]["y"], data[i]["z"]+1)) -- +1 to z because the scanner coordinate for Z is inaccurate.
-    log = log .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
+    log = log .. "\n" .. data[i]["displayName"] .. ": " .. tostring(coords) .. "\n"
    end
   end
  end
- if log ~= "" then
-  logfile.writeLine(os.date("%d.%m.%y %H:%M:%S"))
+ if log ~= os.date("%d.%m.%y %H:%M:%S") then
   logfile.writeLine(log)
   logfile.flush()
  end
